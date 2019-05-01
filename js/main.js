d3919e7748f1e60b5611a574b8cde9ed3abf1cc8
edjunma <junwenma@buffalo.edu>
2019-05-01 02:32:54 -0400

    added match regex, starting on output match to objects

--- js/main.js
+++ js/main.js
@@ -7,7 +7,23 @@ const searchStates = async searchText => {
 	const res = await fetch("../data/states.json")
 	const states = await res.json()
 
-	console.log(states)
+	// Get matches to current text input
+	let matches = states.filter(state => {
+		const regex = new RegExp(`^${searchText}`, "gi")
+		return state.name.match(regex) || state.abbr.match(regex)
+	})
+
+	if (searchText.length === 0) {
+		matches = []
+	}
+
+	outputHtml(matches)
+}
+
+// Show the results in HTML
+const outputHtml = matches => {
+	if (matches.length > 0) {
+	}
 }
 
 search.addEventListener("input", () => searchStates(search.value))
